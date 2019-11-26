dfd70d94cd902723e98838a19bd1abae544a2dcf
paithiov909 <am11.44@gmail.com>
2019-11-26 13:28:58 +0900

    add pkgdown

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,14 @@
-#' Initialization
-#'
-#' @param libname
-#' @param pkgname
+#' onLoad
+#' @param libname libname
+#' @param pkgname pkgname
 #' @importFrom rJava .jpackage
 .onLoad <- function(libname, pkgname)
 {
     rJava::.jpackage(pkgname, lib.loc = libname)
+}
+
+#' onUnload
+.onUnload <- function(libpath)
+{
+    library.dynam.unload("rjavacmecab", libpath)
 }
