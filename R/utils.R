be6dffee5788e374c2a476d1cd1a3af668cbaa03
paithiov909 <am11.44@gmail.com>
2019-12-07 21:26:23 +0900

    v0.1.2-2

--- R/utils.R
+++ R/utils.R
@@ -42,6 +42,7 @@ prettify <- function(list, sep = " ") {
     })
 }
 
+
 #' Ngrams tokenizer
 #'
 #' Make n-gram tokenizer function.
@@ -56,30 +57,29 @@ prettify <- function(list, sep = " ") {
 #'
 #' @import stringi
 #' @export
-ngram_tokenizer <- function(n = 1L,
-             skip_word_none = TRUE,
-             locale = NULL) {
+ngram_tokenizer <- function(n = 1L, skip_word_none = TRUE, locale = NULL)
+{
+    stopifnot(is.numeric(n), is.finite(n), n > 0)
+    options <- stringi::stri_opts_brkiter(type = "word",
+                                          locale = locale,
+                                          skip_word_none = skip_word_none)
 
-        stopifnot(is.numeric(n), is.finite(n), n > 0)
-        options <- stringi::stri_opts_brkiter(type = "word",
-                                              locale = locale,
-                                              skip_word_none = skip_word_none)
+    function(x) {
+        stopifnot(is.character(x))
 
-        function(x) {
-            stopifnot(is.character(x))
+        # Split into word tokens
+        tokens <- unlist(stringi::stri_split_boundaries(x, opts_brkiter = options))
+        len <- length(tokens)
 
-            # Split into word tokens
-            tokens <- unlist(stringi::stri_split_boundaries(x, opts_brkiter = options))
-            len <- length(tokens)
-
-            if (all(is.na(tokens)) || len < n) {
-                # If we didn't detect any words or number of tokens is less than n return empty vector
-                character(0)
-            } else {
-                sapply(1:max(1, len - n + 1), function(i) { stringi::stri_join(tokens[i:min(len, i + n - 1)], collapse = " ") })
-            }
+        if (all(is.na(tokens)) || len < n) {
+            # If we didn't detect any words or number of tokens is less than n return empty vector
+            character(0)
+        } else {
+            sapply(1:max(1, len - n + 1), function(i) { stringi::stri_join(tokens[i:min(len, i + n - 1)], collapse = " ") })
         }
     }
+}
+
 
 #' Return emoji subset
 #'
