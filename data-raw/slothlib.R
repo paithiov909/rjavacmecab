a43ff12912578b45418d8ae651951d24219c9486
paithiov909 <am11.44@gmail.com>
2021-09-04 11:57:42 +0900

    chore: clean project

--- data-raw/slothlib.R
+++ data-raw/slothlib.R
@@ -1,8 +1,7 @@
-library(tidyverse)
-
-#### Slothlib ####
+## code to prepare `slothlib` dataset goes here
 StopWordsJp <- readr::read_csv("http://svn.sourceforge.jp/svnroot/slothlib/CSharp/Version1/SlothLib/NLP/Filter/StopWord/word/Japanese.txt", col_names = FALSE)
 StopWordsJp <- StopWordsJp %>%
     rename(word = X1) %>%
     rowid_to_column()
+
 usethis::use_data(StopWordsJp, overwrite = TRUE)
