7f58e7c3c92a53674522846e89da6f4e54ac1f75
paithiov909 <am11.44@gmail.com>
2022-01-02 00:39:48 +0900

    docs: update pkgdown.yml

--- R/compat-rmecab.R
+++ R/compat-rmecab.R
@@ -5,7 +5,7 @@
 #'
 #' @seealso gbs_freq, gbs_c, gbs_as_tokens, gbs_dfm, gbs_collocate
 #' @rdname gibasa
-#' @name gibasa
+#' @name gbs
 #' @keywords internal
 NULL
 
@@ -15,8 +15,8 @@ NULL
 #' @param ... Other arguments are passed to \code{dplyr::tally()}.
 #' @param .name_repair Logical:
 #' If true, then rename the column names as RMeCabFreq-compatible style.
-#' @returns A data.frame.
-#' @family gibasa
+#' @return A data.frame.
+#' @family gbs
 #' @export
 gbs_freq <- function(df, ..., .name_repair = TRUE) {
   df <- df %>%
@@ -42,16 +42,16 @@ gbs_freq <- function(df, ..., .name_repair = TRUE) {
 #' @param pull A column name of `df`.
 #' @param names A column name of `df`.
 #' @return A list of named vectors.
-#' @family gibasa
+#' @family gbs
 #' @export
 gbs_c <- function(df, pull = c("token", "Original"), names = "POS1") {
   pull <- rlang::arg_match(pull)
-  re <- df %>%
+  res <- df %>%
     dplyr::group_by(.data$doc_id) %>%
     dplyr::group_map(function(.x, .y) {
       purrr::set_names(dplyr::pull(.x, {{ pull }}), dplyr::pull(.x, {{ names }}))
     })
-  return(re)
+  return(res)
 }
 
 #' Pack prettified output as quanteda tokens
@@ -60,7 +60,7 @@ gbs_c <- function(df, pull = c("token", "Original"), names = "POS1") {
 #' @param what Character scalar; which tokenizer to use in \code{quanteda::tokens()}.
 #' @param ... Other arguments are passed to \code{quanteda::tokens()}.
 #' @return A quanteda 'token' class object.
-#' @family gibasa
+#' @family gbs
 #' @export
 gbs_as_tokens <- function(df, pull = "token", n = 1L, sep = "-", what = "fastestword", ...) {
   df <-
@@ -77,8 +77,8 @@ gbs_as_tokens <- function(df, pull = "token", n = 1L, sep = "-", what = "fastest
 #' \code{rjavacmecab::gbs_as_tokens()} and then \code{quatenda::dfm()}.
 #'
 #' @inheritParams gbs_as_tokens
-#' @returns A quanteda 'dfm' object.
-#' @family gibasa
+#' @return A quanteda 'dfm' object.
+#' @family gbs
 #' @export
 gbs_dfm <- function(df, pull = "token", n = 1L, sep = "-", what = "fastestword", ...) {
   res <-
@@ -95,7 +95,7 @@ gbs_dfm <- function(df, pull = "token", n = 1L, sep = "-", what = "fastestword",
 #'
 #' @inheritParams gbs_as_tokens
 #' @return A quanteda 'fcm' object.
-#' @family gibasa
+#' @family gbs
 #' @export
 gbs_collocate <- function(df, pull = "token", n = 1L, sep = "-", what = "fastestword", ...) {
   res <-
